db8fa630844bac2e144b71d2138b139bc230796b
Leonardo Leite <leoinatel@gmail.com>
2023-12-27 16:43:53 -0300

    Feat.: Implementação das funções "matricular" e "main" e refatoração da classe "formação".

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,33 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class Usuario(val nome: String)
 
-class Usuario
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+data class Formacao(
+    val nome: String,
+    var conteudos: List<ConteudoEducacional>,
+    val dificuldade: Nivel
+) {
     val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+
+    fun matricular(vararg usuarios: Usuario) {
+        inscritos.addAll(usuarios)
+        println("${usuarios.size} usuários matriculados na formação $nome")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("Alice")
+    val usuario2 = Usuario("Bob")
+    val usuario3 = Usuario("Carol")
+
+    val conteudo1 = ConteudoEducacional("Kotlin Básico", 5)
+    val conteudo2 = ConteudoEducacional("Kotlin Intermediário", 8)
+
+    val formacaoKotlin = Formacao("Formação Kotlin", listOf(conteudo1, conteudo2), Nivel.INTERMEDIARIO)
+
+    formacaoKotlin.matricular(usuario1, usuario2, usuario3)
 }
